ec4a68efe6436e0c0629f923af0ee4aac8db5b76
Romain Bidault <romain.bidault@supdevinci-edu.fr>
2024-06-19 10:01:48 +0200

    Init spring boot

---
+++ src/main/java/com/night/gather/nightgather/NightgatherApplication.java
@@ -0,0 +1,13 @@
+package com.night.gather.nightgather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightgatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightgatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/night/gather/nightgather/NightgatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.night.gather.nightgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightgatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
