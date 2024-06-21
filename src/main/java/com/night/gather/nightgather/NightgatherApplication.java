c1880b858b3f12400547fb4a67ffb9e0297b67b0
Romain Bidault <romain.bidault@supdevinci-edu.fr>
2024-06-21 14:11:21 +0200

    Do CRUD for Event and adding cache

--- src/main/java/com/night/gather/nightgather/NightgatherApplication.java
+++ src/main/java/com/night/gather/nightgather/NightgatherApplication.java
@@ -2,8 +2,10 @@ package com.night.gather.nightgather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class NightgatherApplication {
 
 	public static void main(String[] args) {
